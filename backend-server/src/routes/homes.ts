ebbc2e45fa7dbda1de8b12d41ce9adee71b488b0
Joe Dinh <j320696@8451.com>
2023-11-04 16:00:30 -0400

    add backend server

--- backend-server/src/routes/homes.ts
+++ backend-server/src/routes/homes.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
-import homesService from '../services/homes';
+import express, { NextFunction, Request, Response } from 'express';
+import homesService from '../services/homes.service';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
@@ -7,7 +7,10 @@ router.get('/', (req: Request, res: Response) => {
   res.json(homes);
 });
 
-router.get('/:id', (req: Request, res: Response) => {
+router.get('/:id', (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error('Not found');
+  err.message = { message: `Home with id: ${req.params['id']} does not exist` };
+  return next(err);
   const home = homesService.getHome(req.params['id']);
   res.json(home);
 });
---
+++ backend-server/src/services/homes.service.ts
@@ -0,0 +1,17 @@
+import homes from '../data/homes-seed.json';
+import { HousingLocation } from '../../../src/app/housing-location/housing-location.interface';
+import homesRepository from '../repository/homes.repository';
+
+class HomesService {
+  getAllHomes() {
+    return homesRepository.getHomes();
+  }
+
+  getHome(homeId: string) {
+    const id = Number(homeId);
+    const home = homesRepository.getHome(id);
+    return home;
+  }
+}
+
+export default new HomesService();
