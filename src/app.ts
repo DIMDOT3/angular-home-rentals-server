748e6722607b338524f1f910c9028006d2191fbb
Joseph Dinh <josephdinh@josephs-mbp.lan>
2024-01-02 17:40:53 -0500

    add health endpoint and gha scheduled workflow

--- src/app.ts
+++ src/app.ts
@@ -14,23 +14,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-// app.get('/homes', (req, res) => {
-//     res.json(locations);
-// });
-
-// app.get('/homes/:id', (req, res) => {
-//     const location = locations.filter(location => location.id === Number(req.params.id));
-//     res.json(location[0]);
-// });
-
-// let myHomes = [];
-// app.get('/my-homes', (req, res) => {
-//   res.json(myHomes);
-// });
-// app.post('/my-homes', (req, res) => {
-//   console.log(req.body);
-//   myHomes.push(req.body);
-//   res.sendStatus(202);
-// });
-
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).send('App is live!')
+});
 export default app;
